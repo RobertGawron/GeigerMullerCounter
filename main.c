d96c5e5581e294cd9bfa1163cdaf3c6af165bb13
RobertGawron <robert_gawron@yahoo.fr>
2014-12-21 19:00:09 +0100

    Dummy data presented on LCD.

--- main.c
+++ main.c
@@ -8,9 +8,14 @@ int main(void) {
 	SystemInit();
 	/* Initialize ILI9341 LCD on board */
 	TM_ILI9341_Init();
-    TM_ILI9341_Fill(ILI9341_COLOR_ORANGE);
+    TM_ILI9341_Fill(ILI9341_COLOR_BLACK);
+    TM_ILI9341_Rotate(TM_ILI9341_Orientation_Landscape_2);
 
-    TM_ILI9341_Puts(90, 310, "uFormicarium v0.1", &TM_Font_7x10, ILI9341_COLOR_BLACK, ILI9341_COLOR_ORANGE);
+    TM_ILI9341_Puts(90, 90, "uFormicarium v0.1", &TM_Font_7x10, ILI9341_COLOR_YELLOW, ILI9341_COLOR_BLACK);
+    TM_ILI9341_Puts(90, 100, "Temperature 30deg", &TM_Font_7x10, ILI9341_COLOR_YELLOW, ILI9341_COLOR_BLACK);
+    TM_ILI9341_Puts(90, 110, "Humidity 40%", &TM_Font_7x10, ILI9341_COLOR_YELLOW, ILI9341_COLOR_BLACK);
+    TM_ILI9341_Puts(90, 120, "Formicarium #1", &TM_Font_7x10, ILI9341_COLOR_YELLOW, ILI9341_COLOR_BLACK);
+
 
 	while(1)
 	{
