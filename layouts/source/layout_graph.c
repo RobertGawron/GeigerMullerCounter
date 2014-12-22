2f471fef5c93c7c290e5d5b54d57e08662a62b1f
RobertGawron <robert_gawron@yahoo.fr>
2014-12-22 21:20:24 +0100

    Files renamed, methods names changed.

--- layouts/source/layout_graph.c
+++ layouts/source/layout_graph.c
@@ -1,11 +1,11 @@
-#include "graph_layout.h"
+#include "layout_graph.h"
 #include "tm_stm32f4_ili9341.h"
 #include "tm_stm32f4_fonts.h"
 #include "tm_stm32f4_stmpe811.h"
 
-void init_graph_layout()
+void layout_graph_init()
 {
-	TM_ILI9341_Fill(ILI9341_COLOR_BLACK);
+	TM_ILI9341_Fill(ILI9341_COLOR_GRAY);
 
 	// draw graph
 	TM_ILI9341_DrawLine(0, ILI9341_WIDTH / 2, ILI9341_HEIGHT, ILI9341_WIDTH / 2,
--- layouts/source/layout_intro.c
+++ layouts/source/layout_intro.c
@@ -1,18 +1,20 @@
-#include "intro_layout.h"
+
+
+#include "layout_intro.h"
 #include "tm_stm32f4_ili9341.h"
 #include "tm_stm32f4_fonts.h"
 #include "tm_stm32f4_stmpe811.h"
 
-void init_intro_layout()
+void layout_intro_init()
 {
-	TM_ILI9341_Fill(ILI9341_COLOR_BLACK);
+	TM_ILI9341_Fill(ILI9341_COLOR_GRAY);
 
 	TM_ILI9341_Puts(90, 90, "uFormicarium v0.1", &TM_Font_7x10,
-			ILI9341_COLOR_YELLOW, ILI9341_COLOR_BLACK);
+			ILI9341_COLOR_BLUE, ILI9341_COLOR_GRAY);
 	TM_ILI9341_Puts(90, 100, "Temperature 30deg", &TM_Font_7x10,
-			ILI9341_COLOR_YELLOW, ILI9341_COLOR_BLACK);
+			ILI9341_COLOR_BLUE, ILI9341_COLOR_GRAY);
 	TM_ILI9341_Puts(90, 110, "Humidity 40%", &TM_Font_7x10,
-			ILI9341_COLOR_YELLOW, ILI9341_COLOR_BLACK);
+			ILI9341_COLOR_BLUE, ILI9341_COLOR_GRAY);
 	TM_ILI9341_Puts(90, 120, "Formicarium #1", &TM_Font_7x10,
-			ILI9341_COLOR_YELLOW, ILI9341_COLOR_BLACK);
+			ILI9341_COLOR_BLUE, ILI9341_COLOR_GRAY);
 }
