4c214f7c7ddeecb5d8c583392381b428585a4ccc
Robert Gawron <robert_gawron@yahoo.fr>
2019-03-24 16:48:25 +0100

    added serial port files

---
+++ software/GeigerCounterFirmware/Inc/serial_port.h
@@ -0,0 +1,13 @@
+/*
+ * serial_port.h
+ *
+ *  Created on: 24.03.2019
+ *      Author: robert
+ */
+
+#ifndef SERIAL_PORT_H_
+#define SERIAL_PORT_H_
+
+
+
+#endif /* SERIAL_PORT_H_ */
---
+++ software/GeigerCounterFirmware/Src/serial_port.c
@@ -0,0 +1,8 @@
+/*
+ * serial_port.c
+ *
+ *  Created on: 24.03.2019
+ *      Author: robert
+ */
+
+
