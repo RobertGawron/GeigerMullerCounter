a27969721164e9db206cc4e848e0ad4646582b81
Robert Gawron <robert_gawron@yahoo.fr>
2020-10-12 15:56:13 +0200

    add python bindings

--- Simulation/main.c
+++ Simulation/main.c
@@ -5,37 +5,25 @@
 #include "gm_measurement_calculator.h"
 #include "gm_logger.h"
 
-
-static void Simulation_Init()
+__declspec(dllexport) void Lib_Simulation_Init()
 {
     GMDisplayUpdater_Init();
     GMMeasurementCalculator_Init();
     GMLogger_Init();
 }
 
-int main()
+__declspec(dllexport) void Lib_GMMeasurementCalculator_OnGMPulseObserved()
 {
-    Simulation_Init();
+    GMMeasurementCalculator_OnGMPulseObserved();
 
-    // invoke some actions ond DUT
-    for(int i=0; i<30; i++)
-    GMMeasurementCalculator_OnGMPulseObserved();
-    GMMeasurementCalculator_OnGMPulseObserved();
-    GMMeasurementCalculator_OnGMPulseObserved();
+    // dummy hack
     GMMeasurementCalculator_OnGMPulseObserved();
     GMMeasurementCalculator_OnSamplingDone();
-
     GMLogger_LogMeasurement();
+}
 
-    // check if possible to retreive data
-    uint8_t *buffer;
-    uint16_t size;
-    GMLoggerSIM_GetLoggedData(&buffer, &size);
 
-    for(int i = 0; i < size; i++)
-    {
-        printf("%c", buffer[i]);
-    }
-
-    printf("\n***\ndone\n");
+__declspec(dllexport) void Lib_GMLoggerSIM_GetLoggedData(uint8_t** buffer, uint16_t* size)
+{
+    GMLoggerSIM_GetLoggedData(buffer, size);
 }
